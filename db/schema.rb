808ff238f03cb9018ff48256472824dcb2d49e89
mikmail <mikael@rund.no>
2022-11-24 11:15:37 +0100

    db fix 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_23_145837) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "followability_relationships", force: :cascade do |t|
     t.string "followerable_type", null: false
@@ -36,13 +36,6 @@ ActiveRecord::Schema.define(version: 2022_11_23_145837) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
