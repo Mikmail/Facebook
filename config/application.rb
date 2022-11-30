05cd4c2f7671c73d84b8e4f2a1ace08be02a54f5
mikmail <mikael@rund.no>
2022-11-30 14:34:49 +0100

    .

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +12,8 @@ module Facebook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.assets.css_compressor = nil
 
     # Configuration for the application, engines, and railties goes here.
     #
