e3f87295c90d366a19d9a5104fe699bec41a2121
mikmail <mikael@rund.no>
2022-12-05 13:23:54 +0100

    Fikset at navn ikke vises

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
       user.email = provider_data.info.email 
+      user.name = provider_data.info.name
       user.password = Devise.friendly_token[0, 20]
     end
   end
